103ac48ced8f784aef1fec062f1eebef53fd45ba
Keith Williams <kwilliams@sunbrandingsolutions.com>
2016-09-16 16:57:44 +0100

    Removing old folders.

--- Deployment/publish-mustache-sharp.bat
+++
@@ -1,4 +0,0 @@
-"C:\Windows\Microsoft.NET\Framework\v4.0.30319\MSBuild.exe" ../mustache-sharp.sln /p:Configuration=Release
-nuget pack ../mustache-sharp/mustache-sharp.csproj -Properties Configuration=Release
-nuget push *.nupkg
-del *.nupkg
